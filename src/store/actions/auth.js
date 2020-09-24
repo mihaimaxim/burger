f00d8ec9d2d70770f62cdaef4141d4df9339d374
Mihai Maxim <mihaimaxim90@icloud.com>
2020-09-24 17:43:48 +0300

    auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -26,18 +26,22 @@ export const authFail = error => {
 export const auth = (email, password, signInMode) => {
    return dispatch => {
       dispatch(authStart())
-
       const authData = {
          email: email,
          password: password,
          returnSecureToken: true,
       }
+<<<<<<< HEAD
 
       let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${key}`
+=======
+      let url =
+         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?' +
+         'key=AIzaSyAKeGX0NAhdZJTE2u_PPuNZROMD7WNFQeA'
+>>>>>>> parent of 0f9c870... auth implementation
       if (signInMode) {
          url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${key}`
       }
-
       axios
          .post(url, authData)
          .then(resp => {
